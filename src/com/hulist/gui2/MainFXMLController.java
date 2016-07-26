fab0b32b6607c22385d62697f6e126a228459f78
Aleksander <Aleksander@samsung_laptop>
2016-07-26 16:35:29 +0200

    działa obsługa list (d'n'd, tooltip)

--- src/com/hulist/gui2/MainFXMLController.java
+++ src/com/hulist/gui2/MainFXMLController.java
@@ -8,6 +8,7 @@ package com.hulist.gui2;
 import com.hulist.logic.chronology.ChronologyFileTypes;
 import com.hulist.logic.chronology.tabs.TabsColumnTypes;
 import com.hulist.logic.climate.ClimateFileTypes;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Locale;
@@ -16,17 +17,27 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javax.swing.DefaultComboBoxModel;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -49,6 +60,10 @@ public class MainFXMLController implements Initializable {
     private ChoiceBox<ChronologyFileTypes> comboBoxChronoFileType;
     @FXML
     private ChoiceBox<ClimateFileTypes> comboBoxClimateFileType;
+    @FXML
+    private ListView<File> listViewDendroFiles;
+    @FXML
+    private ListView<File> listViewClimateFiles;
 
     public static final String MAIN_FXML_NAME = "MainFXML.fxml";
 
@@ -59,9 +74,73 @@ public class MainFXMLController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         titledpane.heightProperty().addListener((obs, oldHeight, newHeight) -> this.guiMain.getMainStage().sizeToScene());
+
+        // ComboBoxes
         comboBoxClimateFileType.setItems(FXCollections.observableArrayList(ClimateFileTypes.values()));
         comboBoxChronoFileType.setItems(FXCollections.observableArrayList(ChronologyFileTypes.values()));
         comboBoxColSelect.setItems(FXCollections.observableArrayList(TabsColumnTypes.values()));
+
+        // Lists
+        Callback<ListView<File>,ListCell<File>> factory = new Callback<ListView<File>, ListCell<File>>() {
+
+            @Override
+            public ListCell<File> call(ListView<File> param) {
+                ListCell<File> cell = new ListCell<File>() {
+                    Tooltip tooltip = new Tooltip();
+
+                    @Override
+                    public void updateItem(File item, boolean empty) {
+                        super.updateItem(item, empty);
+                        if (empty) {
+                            setText(null);
+                            setTooltip(null);
+                        } else {
+                            setText(item.getName());
+                            tooltip.setText(item.getAbsolutePath());
+                            setTooltip(tooltip);
+                        }
+                    }
+                };
+                return cell;
+            }
+        };
+        listViewDendroFiles.setCellFactory(factory);
+        listViewClimateFiles.setCellFactory(factory);
+        listViewDendroFiles.setItems(FXCollections.observableArrayList());
+        listViewClimateFiles.setItems(FXCollections.observableArrayList());
+        listViewDendroFiles.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        listViewClimateFiles.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        EventHandler<DragEvent> doeh = (DragEvent event) -> {
+            if (event.getSource() instanceof ListView) {
+                event.acceptTransferModes(TransferMode.ANY);
+                event.consume();
+            }
+        };
+        listViewDendroFiles.setOnDragOver(doeh);
+        listViewClimateFiles.setOnDragOver(doeh);
+        EventHandler<DragEvent> ddeh = (DragEvent event) -> {
+            if (event.getDragboard().hasFiles()) {
+                if (event.getSource() instanceof ListView) {
+                    ListView<File> source = (ListView<File>) event.getSource();
+                    for (File file : event.getDragboard().getFiles()) {
+                        if (!source.getItems().contains(file)) {
+                            source.getItems().add(file);
+                        }
+                    }
+                }
+            }
+        };
+        listViewDendroFiles.setOnDragDropped(ddeh);
+        listViewClimateFiles.setOnDragDropped(ddeh);
+        EventHandler<KeyEvent> kpeh = (KeyEvent event) -> {
+            if (event.getCode().equals(KeyCode.DELETE) && event.getSource() instanceof ListView) {
+                ListView<File> source = (ListView<File>) event.getSource();
+                source.getItems().removeAll(source.getSelectionModel().getSelectedItems());
+            }
+        };
+        listViewDendroFiles.setOnKeyPressed(kpeh);
+        listViewClimateFiles.setOnKeyPressed(kpeh);
+        
     }
 
     @FXML
